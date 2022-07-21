9d4756b344c3bea72c7803cc47b65bf727d5fef3
Rithika Chavan <91736056+ritzzii@users.noreply.github.com>
2022-07-22 00:56:15 +0530

    Create 1TwoSum.cpp

---
+++ 1TwoSum.cpp
@@ -0,0 +1,63 @@
+1st method:
+--------------------------------------
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) 
+    {
+    unordered_map<int, int> hash;
+	
+    vector<int> result;
+        
+	for (int i = 0; i < numbers.size(); i++) 
+    {
+		int numberToFind = target - numbers[i];
+
+        if (hash.find(numberToFind) != hash.end()) 
+        {
+            
+			result.push_back(hash[numberToFind]);
+			result.push_back(i);			
+			return result;
+		}
+
+        hash[numbers[i]] = i;
+	}
+	return result;
+    }
+};
+//Time Complexity:O(n)
+//Space Complexity:O(n)
+
+2nd method:
+--------------------------------------
+class Solution 
+{
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<nums.size();i++)
+            v.push_back({nums[i],i});
+        sort(v.begin(),v.end());
+        int i=0;
+        int j=v.size()-1;
+        int idx1=0;
+        int idx2=0;
+        while(i<j)
+        {
+            if(v[i].first+v[j].first==target)
+            {
+                idx1=v[i].second;
+                idx2=v[j].second;
+                break;
+                
+            }
+            if(v[i].first+v[j].first<target)
+                i++;
+            if(v[i].first+v[j].first>target)
+                j--;
+    }
+        return {idx1,idx2};
+    }
+//Time Complexity:O(nlogn)
+//Space Complexity:O(n)
